ab0adf52442d48774c00d3518371e50eac581840
Stanislav Stativa <stas.stativa@gmail.com>
2016-06-16 16:06:09 +0300

    added preview for theme

--- code/Sitecore.Speak.Theming/Website/sitecore/shell/client/AdditionalTheme/changeTheme.js
+++ code/Sitecore.Speak.Theming/Website/sitecore/shell/client/AdditionalTheme/changeTheme.js
@@ -1,92 +1,39 @@
-﻿define(["sitecore"], function (sc) {
-  var app = sc.Definitions.App.extend({
-    initialized: function () {     
-      this.ComboBox.on("change:selectedValue", function () {
-        this.ComboBox.off("change:selectedValue");
-        if (this.ComboBox.get("selectedValue") != this.SelectedIdText.get("text")) {
-          this.ComboBox.set("selectedValue", this.SelectedIdText.get("text"));          
-        }
-        
-      }, this);
-    },
+﻿define(["sitecore"],
+    function(sc) {
+        var app = sc.Definitions.App.extend({
+            initialized: function() {
+                this.ComboBox.on("change:selectedValue",
+                    function() {
+                        this.ComboBox.off("change:selectedValue");
+                        if (this.ComboBox.get("selectedValue") != this.SelectedIdText.get("text")) {
+                            this.ComboBox.set("selectedValue", this.SelectedIdText.get("text"));
+                        }
 
-    saveTheme: function () {
-      var item = this.SwitcherDataSource.get("items"),
-              itemModel;
+                    },
+                    this);
 
-      
-      itemModel = item[0].toModel();
-      itemModel.viewModel["Selected theme"](this.ComboBox.get("selectedItemId"));
-      itemModel.sync("update", function () {
-        location.reload();
-      }, {});
-      
+                setTimeout(_.bind(this.changeFrame, this), 1000);
+            },
 
-      
-      
-      
-    }
+            changeFrame: function () {
+                this.Frame.set("isVisible", true);
+                this.Frame.set("width", "100%");
+                this.Frame.set("height", this.Frame.viewModel.$el.contents().height() + "px");
+            },
 
-  });
-  return app;
-});
+            saveTheme: function() {
+                var item = this.SwitcherDataSource.get("items"),
+                    itemModel;
 
+                itemModel = item[0].toModel();
+                itemModel.viewModel["Selected theme"](this.ComboBox.get("selectedItemId"));
+                itemModel.sync("update",
+                    function() {
+                        location.reload();
+                    },
+                    {});
 
-
-
-
-
-/*define(["sitecore"], function (sc) {
-  var app = sc.Definitions.App.extend({
-    initialized: function () {
-      this.ComboBox.on("change:selectedValue", function () {
-        if (this.ComboBox.get("selectedValue") != this.SelectedIdText.get("text") && this.SelectedIdText.get("text") != null) {
-          this.ComboBox.set("selectedValue", this.SelectedIdText.get("text"));
-        }
-
-        if (this.SelectedIdText.get("text") == null) {
-          var item = this.SwitcherDataSource.get("items"), itemModel;
-          itemModel = item[0].toModel();
-          itemModel.viewModel["Selected theme"]("{941BE897-4B7E-4428-87D7-D6CE5662752B}");
-          itemModel.sync("update", function () { }, {});
-          location.reload();
-        }
-
-      }, this);
-    }
-  });
-  return app;
-});*/
-
-/*
-
-define(["sitecore"], function (sc) {
-  var app = sc.Definitions.App.extend({
-    initialized: function () {
-      
-
-      this.ComboBox.on("change:selectedValue", function () {
-        this.ComboBox.get("selectedValue") != this.SelectedIdText.get("text") ?
-          this.ComboBox.set("selectedValue", this.SelectedIdText.get("text")) : $.noop();
-      }, this);
-    },
-    
-    saveTheme: function () {
-      var item = this.SwitcherDataSource.get("items"),
-              itemModel;
-
-      itemModel = item[0].toModel();
-      itemModel.viewModel["Selected theme"]("{941BE897-4B7E-4428-87D7-D6CE5662752B}");
-      itemModel.sync("update", function () {
-        console.log("1");
-      }, {});
-    }
-    
-  });
-  return app;
-});
-
-
-*/
-
-
+            }
+        });
+        return app;
+    });
